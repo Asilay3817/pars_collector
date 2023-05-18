106a09ea2e25525ab5dd1533234269438819bd97
Alex PnW <asilaydying3817@gmail.com>
2023-05-18 20:01:16 +0200

    add mv pars method
    парсинг мв + выгрузка данных в гугл-таблицу

--- pars_mv.py
+++ pars_mv.py
@@ -1,50 +1,76 @@
 import time
-
 from selenium import webdriver
-
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 from selenium.webdriver.chrome.options import Options
 from fake_useragent import UserAgent
-from lxml import etree
+from random import randint
+
+from selenium.webdriver.common.by import By
+
+o = Options()
+ua = UserAgent.random
+o.add_argument("--headless")
+o.add_argument(f'user-agent={ua}')
+driver = webdriver.Chrome(chrome_options=o)
+
+driver.get('https://www.mvideo.ru/smartfony-i-svyaz-10/smartfony-205/f/category=iphone-914/tolko-v-nalichii=da?showCount=12&page=1')
+
+time.sleep(randint(1, 5))
+
+driver.find_element(By.XPATH, "(//button[@class='button button--light button--only-icon button--with-icon ng-star-inserted'])[2]").click()
+big_data = []
+
+for i in range(1, 11):
+    base_url = f'https://www.mvideo.ru/smartfony-i-svyaz-10/smartfony-205/f/category=iphone-914/tolko-v-nalichii=da?showCount=12&page={i}'
+    driver.get(base_url)
+
+    time.sleep(randint(1, 5))
+
+    block = driver.find_element(By.CLASS_NAME, 'plp__card-grid')
 
 
-# o = Options()
-# ua = UserAgent.random
-# o.add_argument("--headless")
-# o.add_argument(f'user-agent={ua}')
-# driver = webdriver.Chrome(chrome_options=o)
-# base_url = 'https://www.mvideo.ru/smartfony-i-svyaz-10/smartfony-205/f/category=iphone-914'
-# driver.get(base_url)
-#
-# time.sleep(11)
-#
-# with open('value_mv.html', 'w', encoding='utf-8') as f:
-#     f.write(driver.page_source)
+    find_end_page = driver.find_element(By.XPATH, "//div[@class='show-more']")
+    webdriver.ActionChains(driver).move_to_element(find_end_page).perform()
+    webdriver.ActionChains(driver).scroll_to_element(find_end_page).perform()
 
-local = "/Users/asilay/Desktop/study/QA/Parcer/value_mv.html"
-#создаем переменную с функцией парсинга html
-html_parser = etree.HTMLParser(encoding='utf-8')
-#создаем переменную в которой хранится локальный файл из которого будем парсить данные используя xpath
-tree = etree.parse(local, html_parser)
 
-#списки в которые добавляем значения после парсинга файла
-items = []
-price = []
 
-for value in tree.xpath('//a/text()'):
-    if "Смартфон Apple iPhone" not in value:
-        continue
-    else:
-        items.append(value)
 
-for value in tree.xpath('//span/text()'):
-    if len(value)<=11:
-        price.append(value)
+    value = block.find_elements(By.PARTIAL_LINK_TEXT, 'Apple iPhone')
+    price = block.find_elements(By.CLASS_NAME, 'price__main-value')
 
-print(items)
-print(price)
+    value_list = []
+    price_list = []
 
-# driver.close()
-# driver.quit()
+    for i in value:
+        value_list.append(i.text)
+    for j in price:
+        price_list.append(j.text.replace(' ₽', ''))
+
+    data = dict(zip(value_list, price_list))
+    big_data.append(data)
+
+
+# поключаем гугл акаунт и гугл таблицы
+scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive "]
+credentials = ServiceAccountCredentials.from_json_keyfile_name('python-pars-kd-f9004d270251.json', scope)
+client = gspread.authorize(credentials)
+# открываем созданную таблицу для записи данных
+sheet = client.open('KD').sheet1
+
+list_to_upload = []
+
+for item in big_data:
+    for k, v in item.items():
+        list_to_upload.append([k, v])
+
+sheet.update("C3:D300", list_to_upload)
+
+
+
+
+
 
 
 
